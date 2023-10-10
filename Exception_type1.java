b408b1248c341ffabe050ab0fd07085095a0fc86
shreyashtalekar458 <147504092+shreyashtalekar458@users.noreply.github.com>
2023-10-10 16:42:02 +0530

    Update Exception_type1.java
    Change class name

--- Exception_type1.java
+++ Exception_type1.java
@@ -1,4 +1,4 @@
-public class Exception_type1 {
+public class Exception_New {
 
     public void ArithmeticExcetion_ex(int a){
         System.out.println("Arithmetic Exception"+a/0);
@@ -6,7 +6,7 @@ public class Exception_type1 {
     }
 
     public static void main(String[] args) {
-        Exception_type1 e1 = new Exception_type1();
+        Exception_New e1 = new Exception_New();
         e1.ArithmeticExcetion_ex(25);
     }
     
